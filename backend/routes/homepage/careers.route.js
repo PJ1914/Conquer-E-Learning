7f48a083e3f9a6994bae371e0e993e89bc0c71cb
mahalaxmi <yelsanimahalaxmi@gmail.com>
2025-11-04 19:39:39 +0530

    Initial commit of careers route for homepage

---
+++ backend/routes/homepage/careers.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  createCareer,
+  getAllCareers,
+  getCareerById,
+  updateCareer,
+  deleteCareer,
+} from "../../controllers/homepage/careers.controller.js";
+
+const router = express.Router();
+
+router.post("/", createCareer);
+router.get("/", getAllCareers);
+router.get("/:id", getCareerById);
+router.put("/:id", updateCareer);
+router.delete("/:id", deleteCareer);
+
+export default router;
+
